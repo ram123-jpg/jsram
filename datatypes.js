53b6412fc3ff4d6be3b82a7468f37a5f3728dbd9
ram123-jpg <ramdevendrayadavyadav@gmail.com>
2025-10-20 10:35:32 +0530

    Initial upload

---
+++ datatypes.js
@@ -0,0 +1,26 @@
+"use strict" // treat all js code as newer version
+
+// alert(3+4) // we are using nodejs, not browser
+
+console.log(3+3)
+
+
+console.log("hitesh")
+
+let name="Ram"
+let age=19
+let isLoggedIn=true
+
+
+// number => 2 to power 53
+// bigint
+//string=""
+//boolean => true/ false
+//null => stanalone value
+// undefined=>
+// symbol => unique
+
+//object
+
+console.log(typeof undefined);// undefined
+console.log(typeof null) // object
---
+++ variables.js
@@ -0,0 +1,17 @@
+const accountId=1352644
+let accountEmail="RAM@GMAIL.COM"
+var accountPassword="12353"
+accountCity="Jaipur"
+//accountId=2// not allowed
+console.log(accountId);
+accountEmail="rc@rc.com"
+accountPassword="3256"
+accountCity="delhi"
+let accountState
+console.log(accountId);
+/*
+ Prefer not to use var because of issue in block scope and functional scope
+
+
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
